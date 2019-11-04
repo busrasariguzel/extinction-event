b6b47f61e837542f9905eb0019b2cf19d0cb958e
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-11-04 08:40:41 -0500

    done

--- main.js
+++ main.js
@@ -12,42 +12,47 @@ function eventListenerLoop () {
 
 eventListenerLoop()
 
+const newUlLi = document.querySelectorAll('.app ul li')
+function changeOpacity(event){
+event.target.style.opacity ='0'
+}
+
+function eventListenerLoop2 () {
+    for (let i=0;i<newUlLi.length;i++) {
+    newUlLi[i].addEventListener('click', changeOpacity);
+}
+}
+
+eventListenerLoop2()
+
+const newImage = document.querySelectorAll('#row img')
+function hide(event){
+event.target.style.width = '0'
+}
+
+function eventListenerLoop3 () {
+    for (let i=0;i<newImage.length;i++) {
+    newImage[i].addEventListener('click', hide);
+}
+}
+
+eventListenerLoop3()
+
+const newButton = document.querySelector('#toggle')
+newButton.addEventListener('click', button)
+
+
+function button(){
+    for (let i=0;i<newUl.length;i++) {
+        newUl[i].style.textDecoration = 'line-through'
+    }
+    for (let i=0;i<newUlLi.length;i++) {
+        newUlLi[i].style.opacity = '0'
+    }
+    for (let i=0;i<newImage.length;i++) {
+        newImage[i].style.width = '0'
+        }
+}
 
 
 
-
-
-
-
-// const secondUlLis = document.querySelectorAll('#secondUl li');
-
-// for (let i = 0; i < secondUlLis.length; i++) {
-//   if (i % 2 === 1) {
-//     secondUlLis[i].style.backgroundColor = 'lightblue';
-//   }
-// }
-
-
-
-
-
-
-
-// function lineThru(event){
-//     event.target.style.textDecoration = 'line-through'
-// }
-// document.querySelectorAll('ol li').addEventListener('click', lineThru)
-// const firstUlLis = document.querySelectorAll('#firstUl li');
-// newOpacity = document.querySelectorAll('.app ul li').addEventListener('click', changeOpacity)
-// function changeOpacity(event){
-//     for (let i = 0; i < newOpacity.length; i++) {
-//     newOpacity[i].style.opacity ='0'
-// }
-// }
-// // document.querySelectorAll('ul').addEventListener('click', changeOpacity)
-
-// function hideImage(event){
-
-//     event.target.style.display = 'none'
-// }
-// newImage = document.querySelectorAll('row').addEventListener('click', hideImage)
